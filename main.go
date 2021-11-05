63571e04f46af586bb4d03d41884cdd590657d42
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-11-05 14:41:58 +0100

    wip

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/oppermax/fhem-go"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"time"
+)
 
-
+type FhemClient struct {
+	url string
+	port int
+	socket string
+	httpClient *http.Client
+}
 
 func main(){
-	log.Info("WIP!")
+	fhem := fhemgo.NewClient("192.168.178.22", 8083)
+
+	//res, err := fhem.httpClient.Get("http://192.168.178.22:8083/fhem?cmd=list%20TYPE=dummy&fwcsrf=csrf_55444420021900")
+	//if err != nil {
+	//	log.WithError(err)
+	//}
+	//b, _ := io.ReadAll(res.Body)
+	//log.Info(string(b))
 }
--- pkg/client.go
+++ pkg/client.go
@@ -1,12 +1,41 @@
-package fhem_go
+package fhemgo
 
-type Client struct{
-	server string
-	port int `default:"1773"`
-	useSsl string `default:"False"`
+import (
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"time"
+)
+
+const csrfPrefix = "&fwcsrf="
+
+type FhemClient struct {
+	client   *http.Client
+	server   string
+	port     int    `default:"1773"`
+	useSsl   string `default:"False"`
 	protocol string `default:"https"`
 	username string
 	password string
-	csrf  bool `default:"True"`
-	catfile string
+	csrf     bool `default:"True"`
+	catfile  string
+}
+
+func NewClient(url string, port int) *FhemClient {
+	return &FhemClient{
+		server: url,
+		port:   port,
+		client: &http.Client{Timeout: time.Minute},
+	}
+}
+
+// FetchToken fetches the current CSRF token which is needed to send commands
+// http://192.168.178.22:8083/fhem?cmd=list%20TYPE=dummy&fwcsrf=csrf_55444420021900
+func (f *FhemClient) FetchToken() {
+	res, err := f.client.Get("http://192.168.178.22:8083/fhem?cmd=tokenconnect")
+	if err != nil {
+		log.WithError(err)
+	}
+
+	res.Header
+
 }
