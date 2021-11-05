4025666d49c5dc0a8c45e7242c4f9de7a11aae98
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-11-05 17:00:37 +0100

    add basic client functions

--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"github.com/oppermax/fhem-go"
-	log "github.com/sirupsen/logrus"
-	"io"
-	"net/http"
-	"time"
+	"github.com/oppermax/fhem-go/pkg"
 )
 
-type FhemClient struct {
-	url string
-	port int
-	socket string
-	httpClient *http.Client
-}
-
 func main(){
-	fhem := fhemgo.NewClient("192.168.178.22", 8083)
+	fhem := fhemgo.NewClient("", 8083, false, true)
+
+	fhem.Get("dummy")
 
 	//res, err := fhem.httpClient.Get("http://192.168.178.22:8083/fhem?cmd=list%20TYPE=dummy&fwcsrf=csrf_55444420021900")
 	//if err != nil {
--- pkg/client.go
+++ pkg/client.go
@@ -1,41 +1,92 @@
 package fhemgo
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"net/url"
 	"time"
 )
 
-const csrfPrefix = "&fwcsrf="
+const fhemCsrfKey = "X-Fhem-Csrftoken"
 
 type FhemClient struct {
-	client   *http.Client
-	server   string
-	port     int    `default:"1773"`
-	useSsl   string `default:"False"`
-	protocol string `default:"https"`
-	username string
-	password string
-	csrf     bool `default:"True"`
-	catfile  string
+	client    *http.Client
+	server    string
+	port      int    `default:"1773"`
+	useSsl    string `default:"False"`
+	protocol  string `default:"https"`
+	username  string
+	password  string
+	csrf      bool `default:"True"`
+	csrfToken string
+	catfile   string
+	baseUrl   string
 }
 
-func NewClient(url string, port int) *FhemClient {
+// NewClient creates a new FHEM client with basic functionality
+func NewClient(server string, port int, https bool, local bool) *FhemClient {
+	client := &http.Client{Timeout: time.Minute}
 	return &FhemClient{
-		server: url,
-		port:   port,
-		client: &http.Client{Timeout: time.Minute},
+		server:    server,
+		port:      port,
+		client:    client,
+		baseUrl:   getBaseUrl(server, port, https, local),
+		csrfToken: fetchToken(client, getBaseUrl(server, port, https, local)),
 	}
 }
 
-// FetchToken fetches the current CSRF token which is needed to send commands
-// http://192.168.178.22:8083/fhem?cmd=list%20TYPE=dummy&fwcsrf=csrf_55444420021900
-func (f *FhemClient) FetchToken() {
-	res, err := f.client.Get("http://192.168.178.22:8083/fhem?cmd=tokenconnect")
-	if err != nil {
+// fetchToken fetches the current CSRF token which is needed to send commands
+func fetchToken(client *http.Client, baseurl string) string {
+	reqUrl := fmt.Sprintf("%s?room=foo", baseurl)
+	res, err := client.Get(reqUrl)
+	if err != nil || res.StatusCode != http.StatusOK {
+		log.WithError(err).Errorf("Could not fetch csrf token from baseurl %s", baseurl)
+	}
+	return res.Header.Get(fhemCsrfKey)
+}
+
+func (f *FhemClient) Get(devType string) {
+	rurl := buildUrl(f.baseUrl, f.csrfToken, "list", devType)
+	res, err := f.client.Get(rurl)
+	if err != nil || res.StatusCode != http.StatusOK {
 		log.WithError(err)
 	}
 
-	res.Header
+}
 
+func buildUrl(base string, token string, cmd string, devType string) string {
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		log.WithError(err).Errorf("Malformed URL: %s", base)
+	}
+
+	params := url.Values{}
+	params.Add("fwcsrf", token)
+	params.Add("cmd", cmd)
+	params.Add("TYPE", devType)
+
+	baseUrl.RawQuery = params.Encode() // Escape Query Parameters
+
+	return baseUrl.String()
+}
+
+func getBaseUrl(server string, port int, https bool, local bool) string {
+	if local {
+		return fmt.Sprintf("localhost:%d/fhem", port)
+	}
+	if https {
+		return fmt.Sprintf("https://%s:%d/fhem", server, port)
+	}
+	return fmt.Sprintf("http://%s:%d/fhem", server, port)
+}
+
+func readBody(response http.Response) (*[]byte, error) {
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.WithError(err).Errorf("Could not read response body %s", response.Body)
+		return nil, err
+	}
+	return &body, nil
 }
